d943a327cfbf157f83bf34083127425e795b1fae
Jason Parent <jason.a.parent@gmail.com>
2019-01-05 21:58:10 -0500

    Part 3, Chapter 1

---
+++ taxi-ui/src/app/components/rider-dashboard/rider-dashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rider-dashboard',
+  templateUrl: './rider-dashboard.component.html'
+})
+export class RiderDashboardComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
+}
