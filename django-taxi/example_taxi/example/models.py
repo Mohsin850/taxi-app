ea8f19f67270451e2aab658ce9d98401ee883138
Jason Parent <jason.a.parent@gmail.com>
2019-01-05 20:07:58 -0500

    Part 1, Chapter 5

--- django-taxi/example_taxi/example/models.py
+++ django-taxi/example_taxi/example/models.py
@@ -8,7 +8,10 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    @property
+    def group(self):
+        groups = self.groups.all()
+        return groups[0].name if groups else None
 
 
 class Trip(models.Model):
